5cc374c282f7dad2ca5bee9b3ff9f71edd8911e3
ohughes343 <owen.hughes271828@gmail.com>
2018-03-10 11:55:13 -0500

    Update orbit.m

--- orbit.m
+++ orbit.m
@@ -1,32 +1,32 @@
 clear all;
 gravity = 6.67e-11; %value of gravitational constant
-satmass = 10; %mass of satellite in kg
-earthmass = 5.972e24; %mass of earth in kg
-earthrad = 6371;%radius of earth in km
-earthtomoon = 384,400; %distance from earth to moon in km
-time = 10; %how long sim will run in seconds
+satmass = 100; %mass of satellite in kg
+earthmass = 5.972e32; %mass of earth in kg
+earthrad = 63.7e7;%radius of earth in m
+earthtomoon = 384e7; %distance from earth to moon in m
+time = 100000; %how long sim will run in seconds
 stepsize = 1;
 steps = time/stepsize; %number of steps that will run
 
 %initial x and y positions
-xpos=[0]; 
-ypos=[2];
+xpos=[8e8]; 
+ypos=[8e8];
 
 %initial x and y velocities
-xvel=[1]; 
-yvel=[1];
+xvel=[5000000]; 
+yvel=[-1000000];
 
 
-r = sqrt((xpos.^2) + (ypos.^2));
+r = sqrt((xpos^2) + (ypos^2));
 
 %initial x and y accelerations
-xacc = [(-1)/(satmass) * (gravity*earthmass*satmass)/(r^2) * (xpos/r)];
-yacc = [(-1)/(satmass) * (gravity*earthmass*satmass)/(r^2) * (ypos/r)];
+xacc(1) = [(-gravity*earthmass*satmass)/(r^3) * (xpos/r)];
+yacc(1) = [(-gravity*earthmass*satmass)/(r^3) * (ypos/r)];
 
 
 figure;
 hold on;
-rectangle('Position', [0 0 2*earthrad 2*earthrad], 'Curvature', [1 1]);
+rectangle('Position', [-earthrad -earthrad 2*earthrad 2*earthrad], 'Curvature', [1 1]);
 daspect([1 1 1]);
 
 
@@ -35,24 +35,25 @@ for n=1:stepsize:time
     
    
     %calculate next position
-    xpos(n+1) = xpos(n) + xvel*stepsize;
-    ypos(n+1) = ypos(n) + yvel*stepsize;
+    xpos(n+1) = xpos(n) + xvel(n)*stepsize;
+    ypos(n+1) = ypos(n) + yvel(n)*stepsize;
     
     %calculate next velocity
-    xvel(n+1) = xvel(n) + xacc*stepsize;
-    yvel(n+1) = yvel(n) + yacc*stepsize;
+    xvel(n+1) = xvel(n) + xacc(n)*stepsize;
+    yvel(n+1) = yvel(n) + yacc(n)*stepsize;
     
-    r=sqrt(xpos(n+1)^2 + ypos(n+1)^2);
+    r=sqrt((xpos(n+1))^2 + (ypos(n+1))^2);
     
-    if(r<earthrad)
-        disp("Crash!")
-    end
-    if(r>earthtomoon)
-        disp("Satellite flew away...")
-    end
+        if(r<earthrad)
+            disp("Crash!")
+        end
         
-    xacc = [(-1)/(satmass) * (gravity*earthmass*satmass)/(r^2) * (xpos(n+1)/r)];
-    yacc = [(-1)/(satmass) * (gravity*earthmass*satmass)/(r^2) * (ypos(n+1)/r)];
+        if(r>earthtomoon)
+            disp("Satellite flew away...")
+        end
+        
+    xacc(n+1) = [(-1)/(satmass) * (gravity*earthmass*satmass)/(r^2) * (xpos(n+1)/r)];
+    yacc(n+1) = [(-1)/(satmass) * (gravity*earthmass*satmass)/(r^2) * (ypos(n+1)/r)];
    
 
 end
